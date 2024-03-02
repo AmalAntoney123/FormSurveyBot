9e582c9ee071677e2867a80ad62e9933a137a8df
AmalAntoney123 <78252452+AmalAntoney123@users.noreply.github.com>
2024-03-02 20:18:02 +0530

    v2

--- formFillBot.py
+++ formFillBot.py
@@ -1,38 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import random
 
-def formFillBot(form_url, button_xpaths1):
-    driver = webdriver.Edge()
-    driver.get(form_url)
-    time.sleep(1)
-
-    for button_list in button_xpaths1:
-        random_index = random.randint(0, len(button_list) - 1)
-        selected_button = driver.find_element(By.XPATH, button_list[random_index])
-        selected_button.click()
-    
-    for button_list in button_xpaths2:
-        num_clicks = random.randint(1, len(button_list))
-        random_indices = random.sample(range(len(button_list)), num_clicks)
-
-        # Click the randomly chosen buttons
-        for index in random_indices:
-            selected_frequent_btn = driver.find_element(By.XPATH, button_list[index])
-            selected_frequent_btn.click()
-
-    #submit button
-    submitXpath = '//*[@id="mG61Hd"]/div[2]/div/div[3]/div/div[1]/div/span/span'
-    submit_btn = driver.find_element(By.XPATH, submitXpath)
-    submit_btn.click()
-    time.sleep(2)
-
-# Replace url with the actual URL of your Google Form
-google_form_url = 'your Google Form link'
-
-# Define your list of XPaths
-button_xpaths1 = [
+# Constants
+LIMIT = 1
+GOOGLE_FORM_URL = 'https://docs.google.com/forms/d/e/1FAIpQLSeV775Ye7gS8n6a7Xf_iauIDR2zoH4l4WrLjfg6nopNfn0mQA/viewform'
+SUBMIT_XPATH = '//*[@id="mG61Hd"]/div[2]/div/div[3]/div/div[1]/div/span/span'
+BUTTON_XPATHS1 = [
     [
         '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div/span/div/div[1]/label/div/div[2]/div/span',
         '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div/span/div/div[2]/label/div/div[2]/div/span',
@@ -117,7 +93,8 @@ button_xpaths1 = [
         '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[17]/div/div/div[2]/div/div/span/div/div[4]/label/div/div[2]/div/span',
     ]
 ]
-button_xpaths2 =[
+
+BUTTON_XPATHS2 = [
     [
         '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div[1]/div[1]/label/div/div[2]/div/span',
         '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div[1]/div[2]/label/div/div[2]/div/span',
@@ -132,7 +109,40 @@ button_xpaths2 =[
     ]
 ]
 
-#enter number of response needed in limit
-limit = 10
-for i in range(0,limit):
-    formFillBot(google_form_url, button_xpaths1)
+
+def wait_and_click(driver, xpath):
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, xpath))).click()
+
+def click_random_buttons(driver, button_list):
+    random_index = random.randint(0, len(button_list) - 1)
+    selected_button = button_list[random_index]
+    wait_and_click(driver, selected_button)
+
+def click_multiple_buttons(driver, button_list):
+    num_clicks = random.randint(1, len(button_list))
+    random_indices = random.sample(range(len(button_list)), num_clicks)
+
+    for index in random_indices:
+        selected_button = button_list[index]
+        wait_and_click(driver, selected_button)
+
+def form_fill_bot(driver, form_url, button_xpaths1, button_xpaths2):
+    driver.get(form_url)
+
+    for button_list in button_xpaths1:
+        click_random_buttons(driver, button_list)
+
+    for button_list in button_xpaths2:
+        click_multiple_buttons(driver, button_list)
+
+    wait_and_click(driver, SUBMIT_XPATH)
+
+def main():
+    driver = webdriver.Edge()
+
+    for _ in range(LIMIT):
+        form_fill_bot(driver, GOOGLE_FORM_URL, BUTTON_XPATHS1, BUTTON_XPATHS2)
+    driver.quit()
+
+if __name__ == "__main__":
+    main()
